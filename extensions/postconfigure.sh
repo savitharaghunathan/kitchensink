326c79ca2ede803b7c498dd61bb32ca766a7f89d
Savitha Raghunathan <6026527+savitharaghunathan@users.noreply.github.com>
2023-08-30 11:47:56 -0400

    Update postconfigure.sh

--- extensions/postconfigure.sh
+++ extensions/postconfigure.sh
@@ -6,6 +6,10 @@ if [ "${SCRIPT_DEBUG}" = "true" ] ; then
     echo "Script debugging is enabled, allowing bash commands and their arguments to be printed as they are executed"
 fi
 
-echo ">>>>>> Executing setup.cli <<<<<<<"
-$JBOSS_HOME/bin/jboss-cli.sh --file=$JBOSS_HOME/extensions/setup.cli
+echo "Appending CLI operations to ${CLI_SCRIPT_FILE}"
+
+echo "
+  /subsystem=datasources/data-source=database_server-DATABASE_SERVER:write-attribute(name=pool-use-strict-min, value=true)
+  /subsystem=datasources/data-source=database_server-DATABASE_SERVER:write-attribute(name=idle-timeout-minutes, value=5)
+" >> "${CLI_SCRIPT_FILE}"
 
